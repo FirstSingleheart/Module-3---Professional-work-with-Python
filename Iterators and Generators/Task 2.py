ccfb7badb0ad891408a72d6304b3124e6ed6c33b
Sergey <Hardsgams@gmail.com>
2021-12-26 21:09:28 +0500

    Task 2 in homework on the "Iterators and Generators"

--- Iterators and Generators/Task 2.py
+++ Iterators and Generators/Task 2.py
@@ -1,4 +1,20 @@
 from typing import Any
 
 
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    [1, 2, None],
+]
+
+
+def flat_generator(my_list: list):
+    new_list = [element for item in my_list for element in item]
+    return new_list
+
+
+for item in flat_generator(nested_list):
+    print(item)
+
+
 
