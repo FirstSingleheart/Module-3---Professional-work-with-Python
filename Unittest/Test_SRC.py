3151d51406608cba428186534b51660a82355317
Sergey <Hardsgams@gmail.com>
2022-01-07 00:51:50 +0500

    Task 1 solution - done

--- Unittest/Test_SRC.py
+++ Unittest/Test_SRC.py
@@ -1,18 +1,16 @@
 import unittest
-from src import get_doc_owner_name, add_new_doc, delete_doc
-from nose_parameterized import parameterized
-
+import src
 
 test_1_data = [
-            ['2207 876234', 'Василий Гупкин'],
-            ['11-2', 'Геннадий Покемонов'],
-            ['10006', 'Аристарх Павлов'],
-            ['19568', None]
+            ('2207 876234', 'Василий Гупкин'),
+            ('11-2', 'Геннадий Покемонов'),
+            ('10006', 'Аристарх Павлов'),
+            ('19568', None)
         ]
 
-test_2_data = ('56789', 'report', 'Arcadiy Pupkin', '3')
+test_2_data = [('56789', 'report', 'Arcentiy Perdidze', '3')]
 
-test_3_data = ("2207 876234", "11-2", "10006")
+test_3_data = ["2207 876234", "11-2", "10006"]
 
 
 class TestSRC(unittest.TestCase):
@@ -20,17 +18,20 @@ class TestSRC(unittest.TestCase):
     def setUp(self) -> None:
         print('Create new_user')
 
-    @parameterized.expand(test_1_data)
-    def test_get_doc_owner_name(self, user_input, answer):
-        self.assertEqual(get_doc_owner_name(user_input), answer)
+    def test_get_doc_owner_name(self):
+        for user_input, answer in test_1_data:
+            with self.subTest():
+                self.assertEqual(src.get_doc_owner_name(user_input), answer)
 
-    @parameterized.expand(test_2_data)
-    def test_add_new_doc(self, number, type_, name, shelf):
-        self.assertEqual(add_new_doc(number, type_, name, shelf), shelf)
+    def test_add_new_doc(self):
+        for number, type_, name, shelf in test_2_data:
+            with self.subTest():
+                self.assertEqual(src.add_new_doc(number, type_, name, shelf), shelf)
 
-    @parameterized.expand(test_3_data)
-    def test_delete_doc(self, number):
-        self.assertTrue(delete_doc(number), number, True)
+    def test_delete_doc(self):
+        for number in test_3_data:
+            with self.subTest():
+                self.assertTrue(src.delete_doc(number))
 
     def tearDown(self) -> None:
         print('Delete new_user')
