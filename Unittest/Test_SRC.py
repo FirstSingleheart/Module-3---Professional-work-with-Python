a5fd5537c0da9ed2eca0874f78ffaf80870b7136
Sergey <Hardsgams@gmail.com>
2022-01-07 00:26:45 +0500

    Task 1 revision homework "Unittest"

--- Unittest/Test_SRC.py
+++ Unittest/Test_SRC.py
@@ -1,20 +1,22 @@
 import unittest
 from src import get_doc_owner_name, add_new_doc, delete_doc
-from parameterized import parameterized
+from nose_parameterized import parameterized
+
 
 test_1_data = [
-            ("2207 876234", "Василий Гупкин"),
-            ("11-2", "Геннадий Покемонов"),
-            ("10006", "Аристарх Павлов"),
-            ("19568", 'Аристарх Пердидзе')
+            ['2207 876234', 'Василий Гупкин'],
+            ['11-2', 'Геннадий Покемонов'],
+            ['10006', 'Аристарх Павлов'],
+            ['19568', None]
         ]
 
 test_2_data = ('56789', 'report', 'Arcadiy Pupkin', '3')
 
-test_3_data = ("2207 876234", "11-2", "10006", "19568")
+test_3_data = ("2207 876234", "11-2", "10006")
 
 
 class TestSRC(unittest.TestCase):
+
     def setUp(self) -> None:
         print('Create new_user')
 
@@ -28,7 +30,7 @@ class TestSRC(unittest.TestCase):
 
     @parameterized.expand(test_3_data)
     def test_delete_doc(self, number):
-        self.assertTrue(delete_doc(number))
+        self.assertTrue(delete_doc(number), number, True)
 
     def tearDown(self) -> None:
         print('Delete new_user')
---
+++ Unittest/Test_alt._src.py
@@ -0,0 +1,42 @@
+import unittest
+from src import delete_doc, add_new_doc, get_doc_owner_name
+
+
+class TestSRC(unittest.TestCase):
+
+    def setUp(self):
+        print('Create new_user')
+
+    def test_get_doc_owner_name(self):
+        self.assertEqual(get_doc_owner_name('2207 876234'), 'Василий Гупкин')
+
+    def test_get_doc_owner_name_1(self):
+        self.assertEqual(get_doc_owner_name('11-2'), 'Геннадий Покемонов')
+
+    def test_get_doc_owner_name_2(self):
+        self.assertEqual(get_doc_owner_name('10006'), 'Аристарх Павлов')
+
+    def test_get_doc_owner_name_3(self):
+        self.assertEqual(get_doc_owner_name('19568'), None)
+
+    def test_add_new_doc(self):
+        self.assertEqual(add_new_doc('56789', 'report', 'Arcadiy Pupkin', '3'), '3')
+
+    def test_delete_doc(self):
+        self.assertEqual(delete_doc('2207 876234'), ('2207 876234', True))
+
+    def test_delete_doc_1(self):
+        self.assertEqual(delete_doc('11-2'), ('11-2', True))
+
+    def test_delete_doc_2(self):
+        self.assertEqual(delete_doc('10006'), ('10006', True))
+
+    # def test_delete_doc_3(self):
+    #     self.assertEqual(delete_doc('19568'), ('19568', None))
+
+    def tearDown(self):
+        print('Delete new_user')
+
+
+if __name__ == '__main__':
+    unittest.main()
